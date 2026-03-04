9468e2e6b6834c7b89fe2211eb502a171ae89ec2
Twrrippy <twrppf@gmail.com>
2026-03-04 22:29:06 +0700

    feat: implement authen, simulate to repo

---
+++ shared/utils/simulate.py
@@ -0,0 +1,17 @@
+import datetime
+from main_system.restaurant import Restaurant
+
+
+class SimulationClock:
+    """Control Over System Time for Testing Purposes"""
+    _current_time = datetime.now()
+
+    @classmethod
+    def set_time(cls, new_time: datetime):
+        cls._current_time = new_time
+        # Trigger time-dependent checks
+        Restaurant.auto_check_no_show()
+
+    @classmethod
+    def get_time(cls):
+        return cls._current_time
