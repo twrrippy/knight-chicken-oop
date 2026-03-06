e6792308dc29bc1dba0052c1d997504f7ff33db1
Preeyanit <preeyanit11262@gmail.com>
2026-03-06 15:47:39 +0700

    Add simulation router and remove advance_time endpoint
    Removed the advance_time endpoint and added simulation_router.

--- main.py
+++ main.py
@@ -10,11 +10,7 @@ from controller.kitchen_controller import router as kitchen_router
 from controller.payment_controller import router as payment_router
 from controller.admin_controller import router as admin_router
 from controller.booking_controller import router as booking_router
-
-from datetime import timedelta
-import uvicorn
-from shared.utils.simulate import SimulationClock
-
+from controller.simulation_controller import router as simulation_router
 
 app = FastAPI()
 mcp = FastMCP()
@@ -25,14 +21,7 @@ app.include_router(kitchen_router)
 app.include_router(payment_router)
 app.include_router(admin_router)
 app.include_router(booking_router)
-
-
-@app.post("/simulate/advance-time", tags=["Simulation"])
-async def advance_time(minutes: int):
-    """advance the simulation clock by specified minutes"""
-    new_time = SimulationClock.get_time() + timedelta(minutes=minutes)
-    SimulationClock.set_time(new_time)
-    return {"current_simulation_time": SimulationClock.get_time()}
+app.include_router(simulation_router)
 
 @app.get("/menu", response_model=dict, tags=["Menu"])
 async def get_menu():
