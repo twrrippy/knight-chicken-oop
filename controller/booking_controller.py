bfd0c690a9c2b77e0a950229f03da8e00f101b96
Twrrippy <twrppf@gmail.com>
2026-03-07 13:50:06 +0700

    update  description for api booking

--- controller/booking_controller.py
+++ controller/booking_controller.py
@@ -23,19 +23,21 @@ async def book_room(
         example={"account_number": "000-0-00000-0"}
     )
 ):
-    """request_booking: There are 5 rooms for booking.\n
-    R01: VIP, Price: 2000 THB/hour\n
-    R02: Hall, Price: 5000 THB/hour\n
-    R03: Standard, Price: 500 THB/hour\n
-    R04: VIP, Price: 2000 THB/hour\n
-    R05: Standard, Price: 500 THB/hour\n
-    room_price = price_per_hour * hours\n
-    deposit = room_price * 50%\n
-    Gold members get 20% discount on room price\n
-    - payment_details: ข้อมูลเพิ่มเติมตามประเภทการจ่ายเงิน เช่น 
-        - qrcode: {'account_number': 'xxx' } 
-        - creditcard: {'card_number': '...', 'cvv': '...'} 
-        - cash: {'cash_received': xxx}\n
+    """# Description: There are 5 rooms for booking.\n
+    **R01**: VIP, Price: 2000 THB/hour\n
+    **R02**: Hall, Price: 5000 THB/hour\n
+    **R03**: Standard, Price: 500 THB/hour\n
+    **R04**: VIP, Price: 2000 THB/hour\n
+    **R05**: Standard, Price: 500 THB/hour\n
+    **room_price** = price_per_hour * hours\n
+    **deposit** = room_price * 50%\n
+    **GOLD members** get 15% discount on room price\n
+    **SILVER members** get 10% discount on room price\n
+    **BRONZE members** get 5% discount on room price\n
+    - **payment_details**: ข้อมูลเพิ่มเติมตามประเภทการจ่ายเงิน เช่น 
+        - qrcode: {"account_number": "xxx"} 
+        - creditcard: {"card_number": "...", "cvv": "..."} 
+        - cash: {"cash_received": xxx}\n
     """
     try:
         payload = restaurant.booking_room(token, member_id, room_id, hours, amount_paid, pay_method, start_time=start_time, payment_details=payment_details)
